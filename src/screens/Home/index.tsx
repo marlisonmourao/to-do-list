a8dae280c6c210e0d84a55b7b383a095bd1c1fae
marlisonmourao <bentesmourao@gmail.com>
2022-10-15 02:05:04 -0400

    types and assets

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { FlatList, Alert, Keyboard } from "react-native";
+import { Container, Form, Wrapped, ContainerList, Separator } from "./styles";
+
+import { Header } from "@components/Header";
+import { Input } from "@components/Input";
+import { ButtonAdd } from "@components/ButtonAdd";
+import { CreatedOrCompleted } from "@components/createdOrCompleted";
+import { TaskCard } from "@components/TaskCard";
+import { ListEmpty } from "@components/ListEmpty";
+
+export function Home() {
+  const [task, setTask] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState("");
+  const [count, setCount] = useState<number>(0);
+
+
+  function handleNewTask() {
+    if (newTask.trim().length === 0) {
+      return Alert.alert("Nova Tarefa", "Digite uma nova tarefa");
+    }
+
+    setTask((prevState) => [...prevState, newTask]);
+    setNewTask("");
+    Keyboard.dismiss();
+  }
+
+  function removeTask(item: string) {
+    if(count !== 0) {
+      setCount(count - 1)
+    }
+    
+    setTask((prevState) => prevState.filter((task) => task !== item))
+    
+  }
+
+  function handleRemoveTask(item: string) {
+    Alert.alert("Remover", "Deseja remover essa tarefa?", [
+      {
+        text: "Sim",
+        onPress: () => removeTask(item)
+          
+      },
+      {
+        text: "Não",
+        style: "cancel",
+      },
+    ]);
+  }
+  
+
+  return (
+    <Container>
+      <Header />
+
+      <Form>
+        <Input
+          onChangeText={setNewTask}
+          value={newTask}
+          onSubmitEditing={handleNewTask}
+        />
+        <ButtonAdd onPress={handleNewTask} icon="plus-circle" />
+      </Form>
+
+      <Wrapped>
+        <CreatedOrCompleted
+          title="Criadas"
+          number={task.length}
+          type="Created"
+        />
+
+        <CreatedOrCompleted 
+          title="Concluídas" 
+          number={count}
+          type="Completed"
+        />
+      </Wrapped>
+
+      {task.length === 0 && <Separator />}
+
+      <ContainerList>
+        <FlatList
+          data={task}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <TaskCard
+              title={item}
+              setCount={setCount}
+              onRemove={() => handleRemoveTask(item)}
+            />
+          )}
+          ListEmptyComponent={() => <ListEmpty />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={[{ paddingBottom: 250 }]}
+        />
+      </ContainerList>
+    </Container>
+  );
+}
